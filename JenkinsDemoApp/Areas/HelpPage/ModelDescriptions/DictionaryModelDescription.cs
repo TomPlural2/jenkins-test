91dbb5eae3ed0fa19b4728f3a5a758b8e0749a8c
Igor Róg <igor.roog@gmail.com>
2016-10-27 22:32:44 +0200

    Add project files.

---
+++ JenkinsDemoApp/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace JenkinsDemoApp.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ JenkinsDemoApp/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace JenkinsDemoApp.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
